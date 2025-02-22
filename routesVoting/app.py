3d1626079f5d694c3721619a2bd276c0f042257f
Omar Pava <omarpava@MacBook-Pro-de-Omar.local>
2025-02-21 23:07:44 -0500

    feat: se conectan las colas y se valida el funcionamiento del reconocimiento de los errores

--- routesVoting/app.py
+++ routesVoting/app.py
@@ -1,6 +1,9 @@
 import pika
 import os
 import json
+import time
+from collections import defaultdict
+import threading  
 
 CLOUDAMQP_URL = os.environ.get(
     'CLOUDAMQP_URL', 'amqps://wvvfljaz:w9AfQi2yFo_obuplLkkV8HjKNrt7GA3M@moose.rmq.cloudamqp.com/wvvfljaz')
@@ -10,34 +13,55 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 voting = []
 
-channel.queue_declare(queue='routes_voting', durable=True)
+queue_state = channel.queue_declare(queue='routes_voting', durable=True)
+voting = defaultdict(list)
+TIMEOUT = 0.5
 
 def callback(ch, method, properties, body):
-    resultado_final = json.loads(body.decode())
-    voting.append(resultado_final)
-    #print(f"✅ Resultado validado recibido: {resultado_final}")
-    validate_vote()
+    final_route = json.loads(body.decode()) 
+    idRoute = final_route[0].get("idRoute", None) if final_route else None
+    
+    voting[idRoute].append(final_route) 
+    if len(voting[idRoute]) == 1:
+        threading.Thread(target=validate_vote, args=(idRoute,), daemon=True).start()
 
-def validate_vote():
-    if len(voting) == 3:
-        serializados = [json.dumps(voto, sort_keys=True) for voto in voting]
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    
 
-        valores_unicos = set(serializados)
+def validate_vote( idRoute ):
+    start_time = time.time()
+    
+    while True:
+        if len(voting[idRoute]) >= 3:
+            print(f"✅ Recibidos 3 votos para {idRoute}: {voting[idRoute][0][0]}")
+            validateUnique( voting[idRoute][0][0], voting[idRoute][1][0], voting[idRoute][2][0] )
+            del voting[idRoute]  
+            break
+        
+        if time.time() - start_time > TIMEOUT:
+            print(f"❌ No llegaron 3 votos para {idRoute} en {TIMEOUT} segundo(s). Se recibieron solo {len(voting[idRoute])} voto(s)")
+            del voting[idRoute] 
+            break
 
-        if len(valores_unicos) == 1:
-            print(f"✅ Los 3 mensajes son IGUALES: {voting[0]}")
-        else:
-            print(f"❌ Hay diferencias. {len(valores_unicos)} valores únicos:")
-            for i, voto in enumerate(voting):
-                print(f"Voto {i+1}: {voto}")
+        time.sleep(0.5)  
 
-        voting.clear()
+def validateUnique(route1, route2, route3):
+    unique_objs = {tuple(obj.items()) for obj in [route1, route2, route3]}
+
+    if len(unique_objs) == 1:
+        print("✅ Los 3 objetos son exactamente iguales")
+    elif len(unique_objs) == 2:
+        print("⚠️ Hay 2 objetos iguales y 1 diferente")
+    else:
+        print("❌ Los 3 objetos son diferentes")
+
 
 channel.basic_consume(
     queue='routes_voting',
     on_message_callback=callback,
-    auto_ack=True
+    auto_ack=False
 )
 
 print("🔄 Esperando mensajes validados...")
 channel.start_consuming()
+
